aad6f46cb7ae2beedeab045f434fb3ba3f2ef6d8
Vanessa Jesik <vajesik@gmail.com>
2023-09-12 11:43:56 -0600

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(f"{i}")
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [integer ** 2 for integer in int_list ]
+    return int_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(101):
+        if i == 0:
+            pass
+        elif i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
